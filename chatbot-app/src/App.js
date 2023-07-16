543ac1c7b2eed29c313f4ef7409911babc69bef4
john7373 <60197372+john7373@users.noreply.github.com>
2023-07-16 15:39:50 +0300

    Update App.js

--- chatbot-app/src/App.js
+++ chatbot-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mon Chatbot</h1>
+        <h1>Mon Chatbot IAGORA</h1>
       </header>
       <main>
         <ChatbotComponent />
